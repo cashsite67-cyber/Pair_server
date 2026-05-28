ebef5dc397bb8c276d68274fe6a291467e52a957
cashsite67-cyber <cashsite67@gmail.com>
2026-05-29 01:19:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/code', async (req, res) => {
             auth: state,
             printQRInTerminal: false,
             logger: pino({ level: 'silent' }),
-            browser: Browsers.ubuntu('Chrome'),
+            browser: Browsers.macOs('Safari')
             generateHighQualityLinkPreview: false,
             connectTimeoutMs: 60000,
             defaultQueryTimeoutMs: 60000,
